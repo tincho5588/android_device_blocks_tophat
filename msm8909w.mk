a9eeb5157fcdaefda858be05a8daf98c55050b09
Bhaskar Borthakur <bhaskarb@codeaurora.org>
2015-09-22 17:31:09 -0700

    Add lunch combo for 8909w for law
    Add parameters relevant for law lunch options
    
    Change-Id: Icd190f3e4a495f428234dc2627f568024f021711

---
+++ msm8909w.mk
@@ -0,0 +1,12 @@
+$(call inherit-product, device/qcom/msm8909/msm8909.mk)
+
+# Flag to be used when applicable for both LW and LAW
+#TARGET_SUPPORTS_WEARABLES := true
+
+PRODUCT_NAME := msm8909w
+PRODUCT_DEVICE := msm8909w
+PRODUCT_BRAND := ANDROID
+
+#PRODUCT_CHARACTERISTICS := nosdcard,watch
+
+#$(call inherit-product, device/google/clockwork/build/wearable-mdpi-512-dalvik-heap.mk)
