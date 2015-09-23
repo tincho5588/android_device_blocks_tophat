2608cad11117fee6f8d6043dc9627fd4abe39ee6
Bhaskar Borthakur <bhaskarb@codeaurora.org>
2015-09-23 16:53:46 -0700

    Update parameters for 8909w lunch options
    Use msm8909 make files from the current directory to make this
    lunch option independent of other lunch options.
    
    Remove un necessary paramters
    
    Change-Id: I07d2a7b9b6e8e67ff933956ade7ab6474a594788

--- msm8909w.mk
+++ msm8909w.mk
@@ -1,11 +1,10 @@
-$(call inherit-product, device/qcom/msm8909/msm8909.mk)
+$(call inherit-product, device/qcom/msm8909w/msm8909.mk)
 
 # Flag to be used when applicable for both LW and LAW
 TARGET_SUPPORTS_WEARABLES := true
 
 PRODUCT_NAME := msm8909w
 PRODUCT_DEVICE := msm8909w
-PRODUCT_BRAND := ANDROID
 
 #PRODUCT_CHARACTERISTICS := nosdcard,watch
 
