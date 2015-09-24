8a2788b00824666a14a06b0e63ed96b28d6ec870
Anurag Singh <anursing@codeaurora.org>
2015-09-23 17:47:11 -0700

    msm8909w: Set TARGET_SUPPORTS_WEARABLES to true
    Set the TARGET_SUPPORTS_WEARALBES flag to true so wearables-specific build
    options can be enabled across projects.
    
    Change-Id: I2bb3d8965159913b33922d21a0fd58a7870f8fb1

--- msm8909w.mk
+++ msm8909w.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/qcom/msm8909/msm8909.mk)
 
 # Flag to be used when applicable for both LW and LAW
-#TARGET_SUPPORTS_WEARABLES := true
+TARGET_SUPPORTS_WEARABLES := true
 
 PRODUCT_NAME := msm8909w
 PRODUCT_DEVICE := msm8909w
